cad7b8706c0425523500d42d51a1a6440f2880bc
Ivan Kononov <ikononov94@gmail.com>
2018-04-05 21:32:35 +0300

    Добавлен класс image__item к тегу img. Поправлены стили Image, для правильного отображения картинок

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -6,6 +6,7 @@ const Image = ({ image, onClick }) =>
   (
     <div className="image">
       <img
+        className="image__item"
         src={image.thumbnailUrl}
         alt={image.name}
         onClick={() => onClick(image.imageId)}
