e8f3731e4c220bf15cf60dee38fdd6fd2926508a
Ivan Kononov <ikononov94@gmail.com>
2018-04-13 17:54:23 +0300

    компонент SlideShow преобразован в контейнер

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { onCloseImage, nextImage, prevImage, loadedImage } from '../../actions/slideShow';
 import './SlideShow.css';
 
-export default class SlideShow extends Component {
+class SlideShow extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      index: null,
-      loading: false,
-    };
-
-    this.nextImage = this.nextImage.bind(this);
-    this.prevImage = this.prevImage.bind(this);
-    this.onCloseSlideShow = this.onCloseSlideShow.bind(this);
     this.onScroll = this.onScroll.bind(this);
   }
 
@@ -21,48 +15,18 @@ export default class SlideShow extends Component {
     document.addEventListener('scroll', this.onScroll);
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      index: nextProps.index,
-      loading: true,
-    });
-  }
-
   componentWillUnmount() {
     document.removeEventListener('scroll', this.onScroll);
   }
 
   onScroll() {
-    if (this.state.index !== null) {
+    if (this.props.index !== null) {
       document.documentElement.scrollTop = 0;
     }
   }
 
-  onCloseSlideShow() {
-    this.setState({
-      index: null,
-    });
-  }
-
-  nextImage() {
-    this.setState({
-      index: this.state.index >= this.props.images.length - 1 ?
-        this.props.images.length - 1 :
-        this.state.index + 1,
-      loading: !(this.state.index >= this.props.images.length - 1),
-    });
-  }
-
-  prevImage() {
-    this.setState({
-      index: this.state.index <= 0 ? 0 : this.state.index - 1,
-      loading: !(this.state.index <= 0),
-    });
-  }
-
   render() {
-    const { images } = this.props;
-    const { index, loading } = this.state;
+    const { images, index, loadingImage } = this.props;
     return (
       index !== null &&
       <div className="show-image">
@@ -71,29 +35,50 @@ export default class SlideShow extends Component {
             role="presentation"
             className="show-image__image"
             src={images[index].contentUrl}
-            onClick={this.nextImage}
-            onLoad={() => this.setState({ loading: false })}
+            onClick={this.props.nextImage}
+            onLoad={this.props.loadedImage}
             alt={images[index].name}
           />
         </div>
-        {loading &&
+        {loadingImage &&
           <div className="loader loader-fullscreen">
             <div className="loader__spin" />
           </div>
         }
-        <div className="show-image__prev" role="presentation" onClick={this.prevImage} />
-        <div className="show-image__next" role="presentation" onClick={this.nextImage} />
-        <div className="show-image__close" role="presentation" onClick={this.onCloseSlideShow}>&times;</div>
+        <div className="show-image__prev" role="presentation" onClick={this.props.prevImage} />
+        <div className="show-image__next" role="presentation" onClick={this.props.nextImage} />
+        <div className="show-image__close" role="presentation" onClick={this.props.onCloseImage}>&times;</div>
       </div>
     );
   }
 }
 
 SlideShow.propTypes = {
-  images: PropTypes.arrayOf(PropTypes.object).isRequired,
+  images: PropTypes.arrayOf(PropTypes.object),
   index: PropTypes.number,
+  loadingImage: PropTypes.bool,
+  nextImage: PropTypes.func.isRequired,
+  prevImage: PropTypes.func.isRequired,
+  loadedImage: PropTypes.func.isRequired,
+  onCloseImage: PropTypes.func.isRequired,
 };
 
 SlideShow.defaultProps = {
+  images: [],
   index: null,
+  loadingImage: false,
 };
+
+export default connect(
+  state => ({
+    images: state.searchReducer.images.value,
+    index: state.slideShowReducer.index,
+    loadingImage: state.slideShowReducer.loadingImage,
+  }),
+  dispatch => ({
+    onCloseImage: () => dispatch(onCloseImage()),
+    nextImage: () => dispatch(nextImage()),
+    prevImage: () => dispatch(prevImage()),
+    loadedImage: () => dispatch(loadedImage()),
+  }),
+)(SlideShow);
