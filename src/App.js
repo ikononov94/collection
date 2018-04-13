33f25514c7f340d6bcfa8101ed19e58bae1b5872
Ivan Kononov <ikononov94@gmail.com>
2018-04-13 17:53:00 +0300

    Компоненту App преобразован в контейнер

--- src/App.js
+++ src/App.js
@@ -1,103 +1,69 @@
-import React, { Component } from 'react';
-import fetch from 'isomorphic-fetch';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchImages, fetchNextImages } from './actions/search';
+import { onClickImage } from './actions/slideShow';
 import SearchForm from './components/SearchForm/SearchForm';
 import Images from './components/Images/Images';
 import SlideShow from './components/SlideShow/SlideShow';
 import Infinite from './components/Infinite/Infinite';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      loading: false,
-      isEmptyInput: false,
-      images: {},
-      index: null,
-      searchValue: '',
-    };
-
-    this.search = this.search.bind(this);
-    this.onClickImage = this.onClickImage.bind(this);
-    this.nextImages = this.nextImages.bind(this);
-  }
-
-  onClickImage(id) {
-    this.state.images.value.forEach((currentValue, currentIndex) => {
-      if (currentValue.imageId === id) {
-        this.setState({
-          index: currentIndex,
-        });
+const App = (props) => {
+  const {
+    errorMessage, images, isFetchingImages, isFetchingNextImages,
+  } = props;
+  return (
+    <section className="collection">
+      <SearchForm fetchImages={props.fetchImages} />
+      {
+        errorMessage &&
+        <p className="error-empty">{errorMessage}</p>
       }
-    });
-  }
-
-  async nextImages() {
-    const { searchValue } = this.state;
-    const { nextOffset, value } = this.state.images;
-    if (value.length !== 150) {
-      const response = await fetch(
-        `https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=${searchValue}&count=${33 + nextOffset}`,
-        { headers: { 'Ocp-Apim-Subscription-Key': 'b763eddd694345cfaa411d94dc428a4c' } },
-      );
-      const images = await response.json();
-      this.setState({ images, index: null });
-    }
-  }
-
-  search(searchValue) {
-    if (!searchValue) {
-      this.setState({ isEmptyInput: true });
-      return;
-    }
-    if (this.state.searchValue === searchValue) return;
-
-    this.setState({ loading: true, isEmptyInput: false, searchValue });
-    fetch(
-      `https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=${searchValue}&count=33`,
-      { headers: { 'Ocp-Apim-Subscription-Key': 'b763eddd694345cfaa411d94dc428a4c' } },
-    )
-      .then(response => response.json())
-      .then((images) => {
-        this.setState({ images, loading: false });
-      })
-      .catch(error => console.log(error));
-  }
-
-  render() {
-    const {
-      loading, images, isEmptyInput,
-    } = this.state;
-    return (
-      <section className="collection">
-        <SearchForm search={this.search} />
-        {
-          isEmptyInput &&
-            <p className="error-empty">Задан пустой поисковый запрос</p>
-        }
-        {
-          loading &&
-            <div className="loader loader-fullscreen">
-              <div className="loader__spin" />
-            </div>
-        }
-        {
-          !isEmptyInput && !loading &&
-          <Infinite nextImages={this.nextImages}>
-            <Images images={images.value} onClick={this.onClickImage} />
-            {Object.keys(images).length !== 0 &&
-              <SlideShow
-                images={this.state.images.value}
-                index={this.state.index}
-              />
-            }
+      {
+        isFetchingImages &&
+        <div className="loader loader-fullscreen">
+          <div className="loader__spin" />
+        </div>
+      }
+      {
+        !errorMessage && !isFetchingImages &&
+          <Infinite nextImages={props.fetchNextImages} isFetching={isFetchingNextImages}>
+            <Images images={images} onClick={props.onClickImage} />
+            <SlideShow />
           </Infinite>
-        }
-      </section>
-    );
-  }
-}
+      }
+    </section>
+  );
+};
 
+App.propTypes = {
+  errorMessage: PropTypes.string,
+  isFetchingImages: PropTypes.bool,
+  isFetchingNextImages: PropTypes.bool,
+  images: PropTypes.arrayOf(PropTypes.object),
+  fetchImages: PropTypes.func.isRequired,
+  fetchNextImages: PropTypes.func.isRequired,
+  onClickImage: PropTypes.func.isRequired,
+};
 
-export default App;
+App.defaultProps = {
+  errorMessage: '',
+  isFetchingImages: false,
+  isFetchingNextImages: false,
+  images: [],
+};
+
+export default connect(
+  state => ({
+    isFetchingImages: state.searchReducer.isFetchingImages,
+    images: state.searchReducer.images.value,
+    errorMessage: state.searchReducer.errorMessage,
+    isFetchingNextImages: state.searchReducer.isFetchingNextImages,
+  }),
+  dispatch => ({
+    fetchImages: searchValue => dispatch(fetchImages(searchValue)),
+    fetchNextImages: () => dispatch(fetchNextImages()),
+    onClickImage: id => dispatch(onClickImage(id)),
+  }),
+)(App);
