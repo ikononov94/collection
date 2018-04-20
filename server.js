b19930c04b2e0708d08a3c91c0158ef237d88c98
Ivan Kononov <ikononov94@gmail.com>
2018-04-20 23:43:19 +0300

    фикс server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.set('port', (process.env.PORT || 5000));
+const PORT = process.env.PORT || 5000;
 
-app.use('/', express.static(__dirname));
+app.use('/*', express.static(__dirname, 'build/index.html'));
 
-app.listen(app.get('port'), () => {
-  console.log(`Server started: http://localhost:${app.get('port')}/`);
+app.listen(PORT, () => {
+  console.log(`Server started: ${port}`);
 });
