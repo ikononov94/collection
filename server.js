b742d7ae0fd3d41fccb62708f5712750dca73432
Ivan Kononov <ikononov94@gmail.com>
2018-04-21 00:04:53 +0300

    Параллельный запуск скриптов изменен на последовательный

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000;
 app.use('/*', express.static(__dirname, 'build/index.html'));
 
 app.listen(PORT, () => {
-  console.log(`Server started: ${port}`);
+  console.log(`Server started: ${PORT}`);
 });
