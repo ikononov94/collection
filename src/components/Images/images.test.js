3c99db5d48a1abbed8595abd19ae7ce0007a1ce7
Ivan Kononov <ikononov94@gmail.com>
2018-04-21 22:08:15 +0300

    Написаны базовые тесты для компонентов Image, Images

---
+++ src/components/Images/images.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import Images from './Images';
+
+const images = [
+  {
+    name: 'image1Name',
+    imageId: 'image1Id',
+    thumbnailUrl: 'image1Url',
+  },
+  {
+    name: 'image1Name',
+    imageId: 'image1Id',
+    thumbnailUrl: 'image1Url',
+  },
+];
+
+const wrapper = shallow(<Images images={images} onClick={() => console.log(Images)} />);
+
+describe('<Images />', () => {
+  it('component Images', () => {
+    expect(wrapper).to.have.length(1);
+  });
+
+  it('should render an `.images`', () => {
+    expect(wrapper.find('.images')).to.have.length(1);
+  });
+});
