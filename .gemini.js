dbb6a23ce7248811977740fbe9665af5f16e5292
Ivan Kononov <ikononov94@gmail.com>
2018-04-23 23:02:06 +0300

    Добавлен конфиг .gemini.js

---
+++ .gemini.js
@@ -0,0 +1,15 @@
+module.exports = {
+    rootUrl: 'http://localhost:3000',
+    gridUrl: 'http://127.0.0.1:4444/wd/hub',
+
+    browsers: {
+        chrome: {
+            calibrate: false,
+            desiredCapabilities: {
+                browserName: 'chrome',
+            },
+            compositeImage: true,
+            screenshotMode: 'viewport',
+        },
+    }
+};
