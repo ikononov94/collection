f06624595552660a538d9711229451650f52810a
Ivan Kononov <ikononov94@gmail.com>
2018-04-23 13:21:59 +0300

    added semicolon

--- src/components/Images/images.test.js
+++ src/components/Images/images.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import sinon from 'sinon'
+import sinon from 'sinon';
 
 import Images from './Images';
 
